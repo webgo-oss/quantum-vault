a02e90b6aff6c0c5ecb48e8e5b0b260c5a4ddf77
Hamza <webgo235@gmail.com>
2026-04-05 17:46:31 +0530

    remoev the files

--- crypto/aes_decrypt.py
+++
@@ -1,9 +0,0 @@
-from cryptography.hazmat.primitives.ciphers.aead import AESGCM
-
-def decrypt_file(encrypted_data, nonce, key):
-
-    aes = AESGCM(key)
-
-    decrypted = aes.decrypt(nonce, encrypted_data, None)
-
-    return decrypted
--- utils/hash_utils.py
+++
@@ -1,9 +0,0 @@
-import hashlib
-
-def generate_hash(data):
-
-    sha = hashlib.sha256()
-
-    sha.update(data)
-
-    return sha.hexdigest()
